b629ddba99818083755739064b900164f5e40cd8
Matthew Precious <matt@mattprecious.com>
2011-09-10 21:01:58 -0700

    Add core functionality
    Specify keyword and alarm.
    Show notification when keyword is detected.

---
+++ src/com/mattprecious/prioritysms/Notification.java
@@ -0,0 +1,109 @@
+package com.mattprecious.prioritysms;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract.PhoneLookup;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Notification extends Activity {
+    
+    private SharedPreferences settings;
+    private MediaPlayer mediaPlayer;
+    
+    private TextView messageView;
+    private Button openButton;
+    private Button dismissButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+        
+        settings        = getSharedPreferences("com.mattprecious.prioritysms_preferences", 0);
+        
+        messageView     = (TextView) findViewById(R.id.message);
+        openButton      = (Button) findViewById(R.id.open);
+        dismissButton   = (Button) findViewById(R.id.dismiss);
+        
+        Intent intent   = getIntent();
+        String number   = intent.getStringExtra("sender");
+        String message  = intent.getStringExtra("message");
+        
+        Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+        
+        String[] columns = new String[]{PhoneLookup.DISPLAY_NAME};
+        Cursor c = getContentResolver().query(uri, columns, null, null, null);
+        
+        String sender;
+        if (c.moveToFirst()) {
+            sender = c.getString(c.getColumnIndex(PhoneLookup.DISPLAY_NAME));
+        } else {
+            sender = number;
+        }
+        
+        setTitle(sender);
+        messageView.setText(message);
+        
+        openButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                stopAlarm();
+                
+                Intent mIntent = new Intent(Intent.ACTION_MAIN);
+                mIntent.setType("vnd.android-dir/mms-sms");
+                
+                startActivity(mIntent);
+                finish();
+            }
+        });
+        
+        dismissButton.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                stopAlarm();
+                finish();
+            }
+        });
+            
+        startAlarm();
+    }
+    
+    private void startAlarm() {
+        String alarm = settings.getString("alarm", null);
+        Uri uri = (alarm == null) ? 
+                        RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM) :
+                        Uri.parse(alarm);
+        
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setDataSource(this, uri);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IllegalArgumentException e) {
+            
+        } catch (IOException e) {
+            
+        }
+    }
+    
+    private void stopAlarm() {
+        mediaPlayer.stop();
+    }
+
+}
---
+++ src/com/mattprecious/prioritysms/SMSReceiver.java
@@ -0,0 +1,45 @@
+package com.mattprecious.prioritysms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Bundle bundle = intent.getExtras();        
+        SmsMessage[] msgs = null;
+        if (bundle != null)
+        {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];            
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);     
+                String sender = msgs[i].getOriginatingAddress();
+                String message = msgs[i].getMessageBody();
+                
+                SharedPreferences settings = context.getSharedPreferences("com.mattprecious.prioritysms_preferences", 0);
+                boolean enabled = settings.getBoolean("enabled", false);
+                String keyword = settings.getString("keyword", "");
+                
+                if (enabled && !keyword.equals("") && message.indexOf(keyword) != -1) {
+                    Intent newIntent = new Intent(context, Notification.class);
+                    
+                    newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    newIntent.putExtra("sender", sender);
+                    newIntent.putExtra("message", message);
+                    
+                    context.startActivity(newIntent);
+                    
+                }
+            }
+        }
+
+    }
+
+}
