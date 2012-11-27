0467b40d8c07b630a0a584011e975c6c5ff9c3b4
Matthew Precious <matt@mattprecious.com>
2012-11-27 00:06:17 -0500

    Alarm on MISSED call

---
+++ PrioritySMS/src/com/mattprecious/prioritysms/CallLogScanner.java
@@ -0,0 +1,100 @@
+package com.mattprecious.prioritysms;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.provider.CallLog;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.PhoneLookup;
+import android.util.Log;
+
+public class CallLogScanner extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("CallLogScanner", "onCreate()");
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        String phoneNumber = null;
+
+        String[] columns = { CallLog.Calls.TYPE, CallLog.Calls.CACHED_NAME, CallLog.Calls.NUMBER };
+        Cursor c = getContentResolver().query(CallLog.Calls.CONTENT_URI, columns, null,
+                null, CallLog.Calls.DEFAULT_SORT_ORDER);
+        try {
+            if (c.moveToFirst()) {
+                if (CallLog.Calls.MISSED_TYPE != c.getInt(c.getColumnIndex(CallLog.Calls.TYPE))) {
+                    return;
+                }
+
+                phoneNumber = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
+            }
+        } finally {
+            c.close();
+        }
+        
+        // TODO: create a helper for this and the SMS receiver
+
+        String contactLookupKey = settings.getString("call_contact", "");
+
+        boolean contactMatch = false;
+
+        // if we're filtering by contact, and
+        // if we're alarming on phone call,
+        // look up the contact id of our filtered contact, and
+        // look up the contact id of the caller, and
+        // check if they're the same ID
+        if (phoneNumber != null && !contactLookupKey.equals("")) {
+            Uri contactUri = Uri.withAppendedPath(Contacts.CONTENT_LOOKUP_URI,
+                    contactLookupKey);
+
+            columns = new String[] { Contacts._ID };
+            c = getContentResolver().query(contactUri, columns, null, null,
+                    null);
+
+            if (c.moveToFirst()) {
+                String contactId = c.getString(c.getColumnIndex(Contacts._ID));
+
+                Uri phoneUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI,
+                        Uri.encode(phoneNumber));
+
+                String[] columns2 = new String[] { PhoneLookup._ID };
+                Cursor c2 = getContentResolver().query(phoneUri, columns2, null,
+                        null, null);
+
+                if (c2.moveToFirst()) {
+                    String thisContactId = c2.getString(c.getColumnIndex(PhoneLookup._ID));
+
+                    contactMatch = thisContactId.equals(contactId);
+                }
+
+                c2.close();
+            }
+
+            c.close();
+        } else {
+            contactMatch = false;
+        }
+
+        if (contactMatch) {
+            Intent newIntent = new Intent(this, Notification.class);
+
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            newIntent.putExtra("sender", phoneNumber);
+            newIntent.putExtra("is_call", true);
+
+            startActivity(newIntent);
+        }
+        
+        Log.d("CallLogScanner", "stopping");
+        stopSelf();
+    }
+}
--- PrioritySMS/src/com/mattprecious/prioritysms/PhoneStateReceiver.java
+++ PrioritySMS/src/com/mattprecious/prioritysms/PhoneStateReceiver.java
@@ -1,85 +1,35 @@
 package com.mattprecious.prioritysms;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.ContactsContract.Contacts;
-import android.provider.ContactsContract.PhoneLookup;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
 public class PhoneStateReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	    Bundle bundle = intent.getExtras();
-	    String phoneNumber= bundle.getString("incoming_number");
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // check if this is an incoming call
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        if (!TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
+            return;
+        }
 
-	    // check if this is an incoming call
-	    String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-	    if (!TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
-	    	return;
-	    }
-
-        // TODO: create a helper for this and the SMS receiver
-
-	    SharedPreferences settings = context.getSharedPreferences(context.getPackageName() + "_preferences", 0);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
 
         boolean enabled = settings.getBoolean("enabled", false);
-        if (!enabled || phoneNumber == null) {
-        	return;
-        }
-
         boolean onCall = settings.getBoolean("on_call", false);
-        String contactLookupKey = settings.getString("call_contact", "");
-
-        boolean contactMatch = false;
-
-        // if we're filtering by contact, and
-        // if we're alarming on phone call,
-        // look up the contact id of our filtered contact, and
-        // look up the contact id of the caller, and
-        // check if they're the same ID
-        if (onCall && !contactLookupKey.equals("")) {
-            Uri contactUri = Uri.withAppendedPath(Contacts.CONTENT_LOOKUP_URI, contactLookupKey);
-
-            String[] columns = new String[] { Contacts._ID };
-            Cursor c = context.getContentResolver().query(contactUri, columns, null, null, null);
-
-            if (c.moveToFirst()) {
-                String contactId = c.getString(c.getColumnIndex(Contacts._ID));
-                
-                Uri phoneUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
-                
-                String[] columns2 = new String[] { PhoneLookup._ID };
-                Cursor c2 = context.getContentResolver().query(phoneUri, columns2, null, null, null);
-                
-                if (c2.moveToFirst()) {
-                    String thisContactId = c2.getString(c.getColumnIndex(PhoneLookup._ID));
-                    
-                    contactMatch = thisContactId.equals(contactId);
-                }
-                
-                c2.close();
-            }
-            
-            c.close();
-        } else {
-            contactMatch = false;
+        if (!enabled || !onCall) {
+            return;
         }
 
-        if (contactMatch) {
-        	Intent newIntent = new Intent(context, Notification.class);
-
-            newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            newIntent.putExtra("sender", phoneNumber);
-            newIntent.putExtra("is_call", true);
-
-            context.startActivity(newIntent);
-        }
-	}
-
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, new Intent(context,
+                CallLogScanner.class), 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 2000, pendingIntent);
+    }
 }
