1d51e91913ccf33e6ae97569bb4d4063074af1ed
Matthew Precious <matt@mattprecious.com>
2013-04-27 15:13:47 -0400

    First pass at new, better notification

--- PrioritySMS/src/com/mattprecious/prioritysms/model/PhoneProfile.java
+++ PrioritySMS/src/com/mattprecious/prioritysms/model/PhoneProfile.java
@@ -1,6 +1,31 @@
 
 package com.mattprecious.prioritysms.model;
 
-public class PhoneProfile extends BaseProfile {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class PhoneProfile extends BaseProfile {
+    public PhoneProfile() {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+    }
+
+    public PhoneProfile(Parcel in) {
+        super(in);
+    }
+
+    public static final Parcelable.Creator<PhoneProfile> CREATOR = new Parcelable.Creator<PhoneProfile>() {
+        @Override
+        public PhoneProfile createFromParcel(Parcel source) {
+            return new PhoneProfile(source);
+        }
+
+        @Override
+        public PhoneProfile[] newArray(int size) {
+            return new PhoneProfile[size];
+        }
+    };
 }
--- PrioritySMS/src/com/mattprecious/prioritysms/model/SmsProfile.java
+++ PrioritySMS/src/com/mattprecious/prioritysms/model/SmsProfile.java
@@ -1,6 +1,11 @@
 
 package com.mattprecious.prioritysms.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.common.collect.Sets;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,5 +30,40 @@ public class SmsProfile extends BaseProfile {
 
     public void removeKeyword(String keyword) {
         keywords.remove(keyword);
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+
+        dest.writeStringArray(keywords.toArray(new String[0]));
+    }
+
+    public SmsProfile(Parcel in) {
+        super(in);
+
+        String[] keywordsArr = in.createStringArray();
+        if (keywordsArr == null) {
+            keywords = Sets.newHashSet();
+        } else {
+            keywords = Sets.newHashSet(keywordsArr);
+        }
+    }
+
+    public static final Parcelable.Creator<SmsProfile> CREATOR = new Parcelable.Creator<SmsProfile>() {
+        @Override
+        public SmsProfile createFromParcel(Parcel source) {
+            return new SmsProfile(source);
+        }
+
+        @Override
+        public SmsProfile[] newArray(int size) {
+            return new SmsProfile[size];
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "SmsProfile [keywords=" + keywords + ", super.toString()=" + super.toString() + "]";
     }
 }
